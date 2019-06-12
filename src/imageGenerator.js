e7c04f50b404d5f25148340e6605cd0eab091341
Dac Chartrand <dac.chartrand@gmail.com>
2019-06-12 17:59:19 -0400

    Consider an init longer than 5 seconds a crash and exit
    @see https://github.com/mathjax/MathJax-node/issues/441

--- src/imageGenerator.js
+++ src/imageGenerator.js
@@ -22,7 +22,9 @@ module.exports.generate = (configs, req, res, next) => {
   function isValidColor(str) {
     return str.match(/^#[a-f0-9]{6}$/i) !== null;
   }
-  myForeground = isValidColor(`#${myForeground}`) ? `#${myForeground}` : '#000000';
+  myForeground = isValidColor(`#${myForeground}`)
+      ? `#${myForeground}`
+      : '#000000';
 
   // Dpi
   dpi = parseInt(dpi);
@@ -40,44 +42,48 @@ module.exports.generate = (configs, req, res, next) => {
   // Convert math into an image
   // --------------------------------------------------------------------------
 
-  // Error image, just in case
+  // Error image
   function formulaDoesNotParse(err) {
     console.error(err);
     res.set('pb-mathjax-error', 'Formula does not parse');
-    return res.sendFile(path.resolve('public/images/formula_does_not_parse.png'));
+    return res.sendFile(
+        path.resolve('public/images/formula_does_not_parse.png'));
   }
 
-  try {
-    mjAPI.config(configs.mathjax);
-    mjAPI.typeset(configs.typeset).then((data) => {
-      if (data.width === '0') {
-        throw new Error('Width equals 0, broken SVG');
-      }
-      // Inject CSS
-      let svg = data.svg;
-      svg = svg.replace(/<title/,
-          `<style>/* <![CDATA[ */ svg { ${svgCss} } /* ]]> */</style><title`);
-      if (isSvg) {
-        // SVG
-        res.set('Content-Type', 'image/svg+xml');
-        return res.send(svg);
-      } else {
-        // PNG
-        sharp(Buffer.from(svg), {density: dpi}).png().toBuffer().then((png) => {
-          res.set('Content-Type', 'image/png');
-          return res.send(png);
-        }).catch((err) => {
-          console.error('There was a problem with Sharp:');
-          return formulaDoesNotParse(err);
-        });
-      }
-    }).catch((err) => {
-      console.error('There was a problem with MathJax.Typeset:');
-      return formulaDoesNotParse(err);
-    });
-  } catch (err) {
-    console.error('There was a problem with MathJax:');
+  // Consider an init longer than 5 seconds a crash and exit
+  const tooLong = setTimeout(() => {
+    // @see https://github.com/mathjax/MathJax-node/issues/441
+    console.error('Too long, Something crashed? Please restart the server.');
+    process.exit(1);
+  }, 5000);
+  mjAPI.config(configs.mathjax);
+  mjAPI.typeset(configs.typeset).then((data) => {
+    clearTimeout(tooLong);
+    if (data.width === '0') {
+      throw new Error('Width equals 0, broken SVG');
+    }
+    // Inject CSS
+    let svg = data.svg;
+    svg = svg.replace(/<title/,
+        `<style>/* <![CDATA[ */ svg { ${svgCss} } /* ]]> */</style><title`);
+    if (isSvg) {
+      // SVG
+      res.set('Content-Type', 'image/svg+xml');
+      return res.send(svg);
+    } else {
+      // PNG
+      sharp(Buffer.from(svg), {density: dpi}).png().toBuffer().then((png) => {
+        res.set('Content-Type', 'image/png');
+        return res.send(png);
+      }).catch((err) => {
+        console.error('There was a problem with Sharp:');
+        return formulaDoesNotParse(err);
+      });
+    }
+  }).catch((err) => {
+    clearTimeout(tooLong);
+    console.error('There was a problem with MathJax.Typeset:');
     return formulaDoesNotParse(err);
-  }
+  });
 
 };
