80c6eedc50652c84e518e1b999a95029b58609ef
Dac Chartrand <dac.chartrand@gmail.com>
2019-07-04 19:04:48 -0400

    Fix STIX-Web patch
    Do not apply to all fonts, just STIX-Web

--- src/myPatches.js
+++ src/myPatches.js
@@ -4,10 +4,20 @@ MathJax.Extension['mypatches'] = {
   version: '0.0.1',
 };
 
-// @see https://github.com/mathjax/MathJax/issues/2035
 MathJax.Hub.Register.StartupHook('SVG Jax Ready', function() {
-  let SVG = MathJax.OutputJax.SVG;
-  SVG.Augment({FONTDATA: {VARIANT: {'bold-italic': {offsetA: 0x1D468}}}});
+  // Helper
+  function checkNested(obj, level, ...rest) {
+    if (obj === undefined) return false;
+    if (rest.length === 0 && obj.hasOwnProperty(level)) return true;
+    return checkNested(obj[level], ...rest);
+  }
+
+  // @see https://github.com/mathjax/MathJax/issues/2035
+  if (checkNested(MathJax, 'AuthorConfig', 'SVG', 'font') &&
+      MathJax.AuthorConfig.SVG.font === 'STIX-Web') {
+    let SVG = MathJax.OutputJax.SVG;
+    SVG.Augment({FONTDATA: {VARIANT: {'bold-italic': {offsetA: 0x1D468}}}});
+  }
 });
 
 MathJax.Callback.Queue(['loadComplete', MathJax.Ajax, '[mypatches]/myPatches.js']);
